87bbbd3d25073fe0182aa5708ae5ad8115af9773
DiegoSanchez <diegosmtattoo@gmail.com>
2025-03-03 18:49:14 -0300

    fix:aceitnado todas origns cors

--- twitter_clone/twitter_clone/settings.py
+++ twitter_clone/twitter_clone/settings.py
@@ -84,10 +84,12 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-"http://localhost:3000",
-'https://twitter-clone-gamma-virid.vercel.app',
-]
+# CORS_ALLOWED_ORIGINS = [
+# "http://localhost:3000",
+# 'https://twitter-clone-gamma-virid.vercel.app',
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
