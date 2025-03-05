5733f403f96af2b5e50a130151a2379535e3af69
DiegoSanchez <diegosmtattoo@gmail.com>
2025-03-05 01:21:36 -0300

    fix: redirecionando para pagina de login

--- frontend/src/app/feed/page.tsx
+++ frontend/src/app/feed/page.tsx
@@ -1,17 +1,25 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Header from "../components/Header";
 import TweetFeed from "../components/TweetFeed";
 
 const FeedPage: React.FC = () => {
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (!token) {
       router.push("/login"); // Redireciona para a página de login se o token não estiver presente
+    } else {
+      setLoading(false); // Conclui o carregamento se o token estiver presente
     }
   }, [router]);
+
+  if (loading) {
+    return <div>Carregando...</div>; // Mostra uma mensagem de carregamento enquanto a verificação está em progresso
+  }
 
   return (
     <div className="min-h-screen bg-gray-100">
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,7 @@
 "use client";
 import LoginPage from "./login/page";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 export default function Home() {
-  const router = useRouter();
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      router.push("/feed"); // Redireciona para a página de login se o token não estiver presente
-    }
-  }, [router]);
-
   return (
     <div>
       <LoginPage></LoginPage>
